1b4891bb2b4be8537c8d2f2a0e4a4caf7ac0d405
Sean <62270910+hawaiiansurfer4@users.noreply.github.com>
2022-03-17 13:25:44 -0700

    initial commit

---
+++ Rides/Rides/RidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidesApp.swift
+//  Rides
+//
+//  Created by Sean Murphy on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct RidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
